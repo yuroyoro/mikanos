642bc22d051ecda2a52f1a473b760faca0d0cf99
Tomohito Ozaki <ozaki@yuroyoro.com>
2021-07-14 08:36:29 +0000

    kernel: implemented printk()

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -25,6 +25,23 @@ void operator delete(void *obj) noexcept
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixleWriter)];
 PixelWriter *pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console *console;
+
+int printk(const char *format, ...)
+{
+	va_list ap;
+	int result;
+	char s[1024];
+
+	va_start(ap, format);
+	result = vsprintf(s, format, ap);
+	va_end(ap);
+
+	console->PutString(s);
+	return result;
+}
+
 extern "C" void KernelMain(
 	const FrameBufferConfig &frame_buffer_config)
 {
@@ -49,13 +66,11 @@ extern "C" void KernelMain(
 		}
 	}
 
-	Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+	console = new (console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-	char buf[128];
 	for (int i = 0; i < 27; ++i)
 	{
-		sprintf(buf, "line %d\n", i);
-		console.PutString(buf);
+		printk("printk %d\n", i);
 	}
 
 	while (1)
