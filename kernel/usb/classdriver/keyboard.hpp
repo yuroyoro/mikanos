28f31929d6c70932b70aa0e10d0349deaefac426
Tomohito Ozaki <ozaki@yuroyoro.com>
2021-07-15 06:52:46 +0000

    kernel: added usb host contoller and device drivers

---
+++ kernel/usb/classdriver/keyboard.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file usb/classdriver/keyboard.hpp
+ *
+ * HID keyboard class driver.
+ */
+
+#pragma once
+
+#include <functional>
+#include "usb/classdriver/hid.hpp"
+
+namespace usb {
+  class HIDKeyboardDriver : public HIDBaseDriver {
+   public:
+    HIDKeyboardDriver(Device* dev, int interface_index);
+
+    void* operator new(size_t size);
+    void operator delete(void* ptr) noexcept;
+
+    Error OnDataReceived() override;
+
+    using ObserverType = void (uint8_t keycode);
+    void SubscribeKeyPush(std::function<ObserverType> observer);
+    static std::function<ObserverType> default_observer;
+
+   private:
+    std::array<std::function<ObserverType>, 4> observers_;
+    int num_observers_ = 0;
+
+    void NotifyKeyPush(uint8_t keycode);
+  };
+}
