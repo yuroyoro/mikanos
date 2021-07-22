0bb468d04ecf62804757d979083aa7f616d4b982
Tomohito Ozaki <ozaki@yuroyoro.com>
2021-07-22 05:26:08 +0000

    kernel: added Layer and LayerManager

---
+++ kernel/layer.cpp
@@ -0,0 +1,101 @@
+#include "layer.hpp"
+
+#include <algorithm>
+
+Layer::Layer(unsigned int id) : id_{id} {
+}
+
+unsigned int Layer::ID() const {
+  return id_;
+}
+
+Layer& Layer::SetWindow(const std::shared_ptr<Window>& window) {
+  window_ = window;
+  return *this;
+}
+
+std::shared_ptr<Window> Layer::GetWindow() const {
+  return window_;
+}
+
+Layer& Layer::Move(Vector2D<int> pos) {
+  pos_ = pos;
+  return *this;
+}
+
+Layer& Layer::MoveRelative(Vector2D<int> pos_diff) {
+  pos_ += pos_diff;
+  return *this;
+}
+
+void Layer::DrawTo(PixelWriter& writer) const {
+  if (window_) {
+    window_->DrawTo(writer, pos_);
+  }
+}
+
+void LayerManager::SetWriter(PixelWriter* writer) {
+  writer_ = writer;
+}
+
+Layer& LayerManager::NewLayer() {
+  ++latest_id_;
+  return *layers_.emplace_back(new Layer{latest_id_});
+}
+
+void LayerManager::Draw() const {
+  for (auto layer : layer_stack_) {
+    layer->DrawTo(*writer_);
+  }
+}
+
+void LayerManager::Move(unsigned int id, Vector2D<int> new_postion) {
+  FindLayer(id)->Move(new_postion);
+}
+
+void LayerManager::MoveRelative(unsigned int id, Vector2D<int> pos_diff) {
+  FindLayer(id)->MoveRelative(pos_diff);
+}
+
+void LayerManager::UpDown(unsigned int id, int new_height) {
+  if (new_height < 0) {
+    Hide(id);
+    return;
+  }
+  if (new_height > layer_stack_.size()) {
+    new_height = layer_stack_.size();
+  }
+
+  auto layer = FindLayer(id);
+  auto old_pos = std::find(layer_stack_.begin(), layer_stack_.end(), layer);
+  auto new_pos = layer_stack_.begin() + new_height;
+
+  if (old_pos == layer_stack_.end()) {
+    layer_stack_.insert(new_pos, layer);
+  }
+
+  if (new_pos == layer_stack_.end()) {
+    --new_pos;
+  }
+  layer_stack_.erase(old_pos);
+  layer_stack_.insert(new_pos, layer);
+}
+
+void LayerManager::Hide(unsigned int id) {
+  auto layer = FindLayer(id);
+  auto pos = std::find(layer_stack_.begin(), layer_stack_.end(), layer);
+  if (pos != layer_stack_.end()) {
+    layer_stack_.erase(pos);
+  }
+}
+
+Layer* LayerManager::FindLayer(unsigned int id) {
+  auto pred = [id](const std::unique_ptr<Layer>& elem) {
+    return elem->ID() == id;
+  };
+  auto it = std::find_if(layers_.begin(), layers_.end(), pred);
+  if (it == layers_.end()) {
+    return nullptr;
+  }
+  return it->get();
+}
---
+++ kernel/layer.hpp
@@ -0,0 +1,89 @@
+/**
+ * @file layer.hpp
+ *
+ * 重ね合わせ処理を提供する。
+ */
+
+#pragma once
+
+#include <map>
+#include <memory>
+#include <vector>
+
+#include "graphics.hpp"
+#include "window.hpp"
+
+/** @brief Layer は 1 つの層を表す。
+ *
+ * 現状では 1 つのウィンドウしか保持できない設計だが，
+ * 将来的には複数のウィンドウを持ち得る。
+ */
+class Layer {
+public:
+  /** @brief 指定された ID を持つレイヤーを生成する。 */
+  Layer(unsigned int id = 0);
+
+  /** @brief このインスタンスの ID を返す。 */
+  unsigned int ID() const;
+
+  /** @brief ウィンドウを設定する。既存のウィンドウはこのレイヤーから外れる。 */
+  Layer& SetWindow(const std::shared_ptr<Window>& window);
+
+  /** @brief 設定されたウィンドウを返す。 */
+  std::shared_ptr<Window> GetWindow() const;
+
+  /** @brief レイヤーの位置情報を指定された絶対座標へと更新する。再描画はしない。 */
+  Layer& Move(Vector2D<int> pos);
+
+  /** @brief レイヤーの位置情報を指定された相対座標へと更新する。再描画はしない。 */
+  Layer& MoveRelative(Vector2D<int> pos_diff);
+
+  /** @brief writer に現在設定されているウィンドウの内容を描画する。 */
+  void DrawTo(PixelWriter& writer) const;
+
+private:
+  unsigned int id_;
+  Vector2D<int> pos_;
+  std::shared_ptr<Window> window_;
+};
+
+/** @brief LayerManager は複数のレイヤーを管理する。 */
+class LayerManager {
+public:
+  /** @brief Draw メソッドなどで描画する際の描画先を設定する。 */
+  void SetWriter(PixelWriter* writer);
+
+  /** @brief 新しいレイヤーを生成して参照を返す。
+   *
+   * 新しく生成されたレイヤーの実体は LayerManager 内部のコンテナで保持される。
+   */
+  Layer& NewLayer();
+
+  /** @brief 現在表示状態にあるレイヤーを描画する。 */
+  void Draw() const;
+
+  /** @brief レイヤーの位置情報を指定された絶対座標へと更新する。再描画はしない。 */
+  void Move(unsigned int id, Vector2D<int> new_position);
+
+  /** @brief レイヤーの位置情報を指定された相対座標へと更新する。再描画はしない。 */
+  void MoveRelative(unsigned int id, Vector2D<int> pos_diff);
+
+  /** @brief レイヤーの高さ方向の位置を指定された位置に移動する。
+   *
+   * new_height に負の高さを指定するとレイヤーは非表示となり，
+   * 0 以上を指定するとその高さとなる。
+   * 現在のレイヤー数以上の数値を指定した場合は最前面のレイヤーとなる。
+   * */
+  void UpDown(unsigned int id, int new_hight);
+  void Hide(unsigned int id);
+
+private:
+  PixelWriter* writer_{nullptr};
+  std::vector<std::unique_ptr<Layer>> layers_{};
+  std::vector<Layer*> layer_stack_{};
+  unsigned int latest_id_{0};
+
+  Layer* FindLayer(unsigned int id);
+};
+
+extern LayerManager* layer_manager;
